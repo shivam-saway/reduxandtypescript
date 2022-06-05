a116d839f8d01671bbd8fbdd939556a09e8c93c8
= <=>
2022-06-05 22:47:00 +0530

    Reduc store implementation-simple app to serch pkg

---
+++ src/components/RepositoriesList.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { useActions } from "../hooks/useActions";
+import { useTypedSelector } from "../hooks/useTypedSelector";
+const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState("");
+  const { SearchRepositories } = useActions();
+  const { data, error, loading } = useTypedSelector(
+    (state) => state.repositories
+  );
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    SearchRepositories(term);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
+        <button>Search</button>
+      </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map((name) => <div key={name}> {name} </div>)}
+    </div>
+  );
+};
+
+export default RepositoriesList;
